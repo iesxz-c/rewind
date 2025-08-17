2ba9f20efb3054fcbb8ef178e765141fc886c5ef
Akash <akash.krishnan.046@gmail.com>
2025-08-17 18:19:58 +0530

    reconfig

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,10 @@ export default function Index() {
     <View
      className="flex-1 items-center justify-center bg-green-100"
     >
-      <Text className="text-red-900 font-bold  text-9xl">
-  Hello with hex 🎨
+      <Text className="text-red-900 font-quicksand-light  text-9xl">
+  Hello with hex
+  Akash 
+  🎨
 </Text>
  </View>
   );
