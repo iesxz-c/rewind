30c4d8d814cf6774a07b074218c84ce82a7b5ed3
Akash <akash.krishnan.046@gmail.com>
2025-08-17 16:10:59 +0530

    fixing tailwind issues

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 items-center justify-center"
     >
-      <Text>HI</Text>
+      <Text className="text-2xl font-bold text-teal-950">HI</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",        // main App file
+    "./app/**/*.{js,jsx,ts,tsx}",   // all files in /app (Expo Router structure)
+    "./components/**/*.{js,jsx,ts,tsx}", // any components folder
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
